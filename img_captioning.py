568ab7e1ed8246af2850737381e62db343c5ea55
Sohamtapase2004 <sohamtapase2004@gmail.com>
2024-06-16 10:22:10 +0530

    Add files via upload
    Program file added "img_captioning"

---
+++ img_captioning.py
@@ -0,0 +1,169 @@
+import os
+
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+import keras
+from keras import layers
+from keras.applications import efficientnet
+from keras.layers import TextVectorization
+
+keras.utils.set_random_seed(111)
+
+# Path to the images
+IMAGES_PATH = "Flicker8k_Dataset"
+
+# Desired image dimensions
+IMAGE_SIZE = (299, 299)
+
+# Vocabulary size
+VOCAB_SIZE = 10000
+
+# Fixed length allowed for any sequence
+SEQ_LENGTH = 25
+
+# Dimension for the image embeddings and token embeddings
+EMBED_DIM = 512
+
+# Per-layer units in the feed-forward network
+FF_DIM = 512
+
+# Other training parameters
+BATCH_SIZE = 64
+EPOCHS = 30
+AUTOTUNE = tf.data.AUTOTUNE
+
+# ______________________________________ Preparing dataset ____________________________________________________________________________
+def load_captions_data(filename):
+    """Loads captions (text) data and maps them to corresponding images.
+
+    Args:
+        filename: Path to the text file containing caption data.
+
+    Returns:
+        caption_mapping: Dictionary mapping image names and the corresponding captions
+        text_data: List containing all the available captions
+    """
+
+    with open(filename) as caption_file:
+        caption_data = caption_file.readlines()
+       # print(len(caption_data)) # changes made here______________________________________________________
+        caption_mapping = {}
+        text_data = []
+        images_to_skip = set()
+
+        for line in caption_data:
+            line = line.rstrip("\n")
+            # Image name and captions are separated using a tab
+            img_name, caption = line.split("\t")
+
+            # Each image is repeated five times for the five different captions.
+            # Each image name has a suffix `#(caption_number)`
+            img_name = img_name.split("#")[0]
+            img_name = os.path.join(IMAGES_PATH, img_name.strip())
+
+            # We will remove caption that are either too short to too long
+            tokens = caption.strip().split()
+
+            if len(tokens) < 5 or len(tokens) > SEQ_LENGTH:
+                images_to_skip.add(img_name)
+                continue
+
+            if img_name.endswith("jpg") and img_name not in images_to_skip:
+                # We will add a start and an end token to each caption
+                caption = "<start> " + caption.strip() + " <end>"
+                text_data.append(caption)
+                # print("text_data",text_data) # changes made here______________________________________________________
+
+                if img_name in caption_mapping:
+                    caption_mapping[img_name].append(caption)
+                    # print(caption_mapping.keys(),caption_mapping[img_name]) # changes made here______________________________________________________
+                else:
+                    caption_mapping[img_name] = [caption]
+                    # print(caption_mapping.keys(),caption_mapping[img_name]) # changes made here______________________________________________________
+
+
+        for img_name in images_to_skip:
+            if img_name in caption_mapping:
+                del caption_mapping[img_name]
+        # print(caption_mapping) #changes made here _________________________________________________________
+        return caption_mapping, text_data
+
+
+def train_val_split(caption_data, train_size=0.8, shuffle=True):
+    """Split the captioning dataset into train and validation sets.
+
+    Args:
+        caption_data (dict): Dictionary containing the mapped caption data
+        train_size (float): Fraction of all the full dataset to use as training data
+        shuffle (bool): Whether to shuffle the dataset before splitting
+
+    Returns:
+        Traning and validation datasets as two separated dicts
+    """
+
+    # 1. Get the list of all image names
+    all_images = list(caption_data.keys())
+    # print(all_images);#changes made here ________________________________________________________________________________________
+
+    # 2. Shuffle if necessary
+    if shuffle:
+        np.random.shuffle(all_images)
+    # print(len(caption_data))#changes made here ________________________________________________________________________________________
+    # 3. Split into training and validation sets
+    train_size = int(len(caption_data) * train_size)
+    # print(train_size)#changes made here ________________________________________________________________________________________
+    training_data = {
+        img_name: caption_data[img_name] for img_name in all_images[:train_size]
+    }
+    print(training_data) #changes made here ________________________________________________________________________________________
+
+    validation_data = {
+        img_name: caption_data[img_name] for img_name in all_images[train_size:]
+    }
+
+    # 4. Return the splits
+    return training_data, validation_data
+
+
+# Load the dataset
+captions_mapping, text_data = load_captions_data("Flickr8k.token.txt")
+# print(len(text_data)) changes made here ________________________________________________________________________________________
+
+# Split the dataset into training and validation sets
+train_data, valid_data = train_val_split(captions_mapping)
+print("Number of training samples: ", len(train_data))
+print("Number of validation samples: ", len(valid_data))
+
+
+# ______________________________________ Preparing dataset ____________________________________________________________________________
+def custom_standardization(input_string):
+    lowercase = tf.strings.lower(input_string)
+    return tf.strings.regex_replace(lowercase, "[%s]" % re.escape(strip_chars), "")
+
+
+strip_chars = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+strip_chars = strip_chars.replace("<", "")
+strip_chars = strip_chars.replace(">", "")
+
+vectorization = TextVectorization(
+    max_tokens=VOCAB_SIZE,
+    output_mode="int",
+    output_sequence_length=SEQ_LENGTH,
+    standardize=custom_standardization,
+)
+vectorization.adapt(text_data)
+
+# Data augmentation for image data
+image_augmentation = keras.Sequential(
+    [
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.2),
+        layers.RandomContrast(0.3),
+    ]
+)
+print(custom_standardization("'Flicker8k_Dataset/3459362347_c412ef9901.jpg': ['<start> A woman and two elderly men are standing in front of a speaker . <end>']"))
